bdffbdf3d0ba3bbf5d69c5d54569652e044c093c
yamayamaKo <yamachanwow@gmail.com>
2026-03-01 16:17:26 +0900

    first commit

---
+++ src/main/kotlin/com/yamada/openapi_generator/OpenapiGeneratorApplication.kt
@@ -0,0 +1,11 @@
+package com.yamada.openapi_generator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenapiGeneratorApplication
+
+fun main(args: Array<String>) {
+	runApplication<OpenapiGeneratorApplication>(*args)
+}
---
+++ src/test/kotlin/com/yamada/openapi_generator/OpenapiGeneratorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.yamada.openapi_generator
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenapiGeneratorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
